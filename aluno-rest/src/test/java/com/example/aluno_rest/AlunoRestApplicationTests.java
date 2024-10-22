e08b4b16347416bd71839666887dc287272e897d
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-10-22 11:14:10 -0300

    :tada: adiciona projeto Maven para ser a camada REST da arquitetura

---
+++ aluno-rest/src/test/java/com/example/aluno_rest/AlunoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aluno_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
